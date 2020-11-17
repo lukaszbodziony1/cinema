d6bd84f70e4c79f3fb329d1336c52c150ff5270c
lukasz_bodziony <lukasz.bodziony96@gmail.com>
2020-11-17 18:47:58 +0100

    B8 Get movie/movies

--- src/main/java/com/ssi/cinema/controller/MovieController.java
+++ src/main/java/com/ssi/cinema/controller/MovieController.java
@@ -1,13 +1,19 @@
 package com.ssi.cinema.controller;
 
+import com.ssi.cinema.dto.MovieDto;
+import com.ssi.cinema.request.common.CommonGetRequest;
 import com.ssi.cinema.request.movie.AddMovieRequest;
 import com.ssi.cinema.service.MovieService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/movie")
@@ -21,10 +27,25 @@ public class MovieController {
 
     @PostMapping("/add")
     public ResponseEntity<String> addNewGenre(@RequestBody AddMovieRequest request) {
-
         service.addNewMovie(request);
 
         return ResponseEntity.ok()
                 .body("Successfully created new movie!");
+    }
+
+    @PostMapping("/get")
+    public ResponseEntity<List<MovieDto>> getMovies(@RequestBody CommonGetRequest request) {
+        List<MovieDto> movies = service.getAllMovies(request);
+
+        return ResponseEntity.ok()
+                .body(movies);
+    }
+
+    @GetMapping("/get/{id}")
+    public ResponseEntity<MovieDto> getMovieById(@PathVariable int id) {
+        MovieDto movie = service.getMovieById(id);
+
+        return ResponseEntity.ok()
+                .body(movie);
     }
 }
--- src/main/java/com/ssi/cinema/repository/MovieRepository.java
+++ src/main/java/com/ssi/cinema/repository/MovieRepository.java
@@ -3,6 +3,9 @@ package com.ssi.cinema.repository;
 import com.ssi.cinema.entity.Genre;
 import com.ssi.cinema.entity.Movie;
 import com.ssi.cinema.exception.genric.CreatingNewObjectException;
+import com.ssi.cinema.exception.genric.GetSingleObjectException;
+import com.ssi.cinema.exception.genric.GettingObjectsException;
+import com.ssi.cinema.request.common.CommonGetRequest;
 import com.ssi.cinema.request.movie.AddMovieRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.hibernate.Session;
@@ -10,6 +13,8 @@ import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 @Slf4j
@@ -55,5 +60,61 @@ public class MovieRepository extends CommonRepository {
             session.close();
             factory.close();
         }
+    }
+
+    public List<Movie> getAllMovies(CommonGetRequest request) {
+        SessionFactory factory = new Configuration()
+                .configure()
+                .addAnnotatedClass(Movie.class)
+                .addAnnotatedClass(Genre.class)
+                .buildSessionFactory();
+        Session session = factory.getCurrentSession();
+
+        List<Movie> movies;
+        String query = buildGetQuery(request.getFilters(), request.getOrder(), "Movie");
+
+        log.info("Getting movies from database...");
+        try {
+            session.beginTransaction();
+
+            movies = session.createQuery(query).getResultList();
+            session.getTransaction().commit();
+
+            log.info("Movies successfully get!");
+        } catch (Exception e) {
+            log.error("Error while getting movies from database!", e);
+            throw new GettingObjectsException("There was a problem while processing movies from database! " + e);
+        } finally {
+            session.close();
+            factory.close();
+        }
+
+        return movies;
+    }
+
+    public Movie getMovieById(int id) {
+        SessionFactory factory = new Configuration()
+                .configure()
+                .addAnnotatedClass(Movie.class)
+                .addAnnotatedClass(Genre.class)
+                .buildSessionFactory();
+        Session session = factory.getCurrentSession();
+
+        Movie movie;
+
+        log.info("Getting movie with id " + id + " from database...");
+        try {
+            session.beginTransaction();
+            movie = session.get(Movie.class, id);
+        } catch (Exception e) {
+            log.error("Error while getting movie with id " + id +" from database!", e);
+            throw new GetSingleObjectException("Error while getting movie with id " + id +" from database!");
+        } finally {
+            session.close();
+            factory.close();
+        }
+        log.info("Successfully get movie with id " + id + " from database!");
+
+        return movie;
     }
 }
